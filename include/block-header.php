dfedcc56c87afafbfe1958a31a0b71500056fad6
wveik <kataev_06@list.ru>
2015-11-19 12:22:56 +0400

    Первый commit

---
+++ include/block-header.php
@@ -0,0 +1,36 @@
+<div id="block-header">
+	<div id="header-top-block">
+		<ul id="header-top-menu">
+			<li>Ваш город - <span>Москва</span></li>
+			<li><a href="o-nas.php">О нас</a></li>
+			<li><a href="magaziny.php">Магазины</a></li>
+			<li><a href="feedback.php">Контакты</a></li>
+		</ul>
+
+
+		
+		<p id="reg-auth-title" align="right">
+			<a class="top-auth" href="">Вход</a>
+			<a href="registration.php">Регистрация</a>
+		</p>
+
+	</div>
+	
+
+
+	<div id="top-line"></div>
+
+	<!-- Логотип -->
+	<img id="img-logo" src="images/logo.png" />
+
+	<div id="personal-info">
+		<p align="right">Звонок бесплатный.</p>
+			<h3 align="right">8 (800) 100-12-34</h3>
+			<img src="/images/phone-icon.png" />
+		<p align="right">Режим работы:</p>
+		<p align="right">Будние дни: с 9:00 до 18:00</p>
+		<p align="right">Суббота, Воскресенье - выходные</p>
+			<img src="/images/time-icon.png" />
+	</div>
+
+</div>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Интернет-Магазин Серебро</title>
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="css/reset.css" />
+</head>
+<body>
+
+<div id="block-body">
+	
+<?php 
+	include("include/block-header.php")
+?>
+
+<div id="block-right"></div>
+
+
+<div id="block-content"></div>
+
+
+<?php 
+	include("include/block-footer.php")
+?>
+
+</div>
+	
+</body>
+</html>
